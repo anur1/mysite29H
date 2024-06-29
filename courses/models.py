14fe782a41e1b58befdf103bff4a17309bd8a968
A Nuri <ahmettonuri@gmail.com>
2024-06-29 16:09:22 +0300

    reload

--- courses/models.py
+++ courses/models.py
@@ -7,19 +7,22 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default = "", null=False)
+    slug = models.SlugField(default = "", null=False, blank=True, editable=False, unique=True, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug=slugify(self.title)
         super().save(args, kwargs)
 
     def __str__(self):
-        return f"{self.title} {self.date}"
+        return f"{self.title}"
 
 
 class Category(models.Model):
     name = models.CharField( max_length=50)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
     
 
 
