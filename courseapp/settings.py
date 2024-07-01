605cc5bc533a3a8cf9912bb83fa3421ea3de74a0
anur1 <ahmettonuri@gmail.com>
2024-07-01 17:37:04 +0300

    comment added to settings.py

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+#STATIC_ROOT = '/var/www/static'  veya  '/static' ardından mysite içinde python manage.py collectstatic
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
