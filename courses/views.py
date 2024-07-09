2127a7b6b991379666904327ade565df11ffbeea
anur1 <ahmettonuri@gmail.com>
2024-07-09 15:43:11 +0300

    get request

--- courses/views.py
+++ courses/views.py
@@ -7,20 +7,37 @@ from django.core.paginator import Paginator
 
 
 def index(request):
-
     kurslar = Course.objects.filter(isActive=1)
     kategoriler = Category.objects.all()
-
-
 #    for kurs in db["courses"]:
 #         if kurs["isActive"] == True: 
 #             kurslar.append(kurs)
-
-
-
     return render(request, 'courses/index.html', {
         'categories': kategoriler,
         'courses': kurslar
+    })
+
+def search(request):
+    if "q" in request.GET and request.GET["q"] !="":
+        q = request.GET["q"]
+        kurslar  = Course.objects.filter(isActive = True, title__contains=q).order_by("date")#category__slug = slug,
+        kategoriler = Category.objects.all()
+    else:
+        return redirect("/kurslar")  
+
+    
+    paginator = Paginator(kurslar, 3) #filtrelenmiş kursları her sayfada 5'er adet göster
+    page = request.GET.get('page', 1) #query'den gelen page number'ı kullan
+    page_obj = paginator.page(page) #page e düşen kursları pageob'e at
+    
+    print(page_obj.paginator.count) #toplam ürün sayısı
+    print(page_obj.paginator.num_pages) # toplam sayfa sayısı
+
+
+    return render(request, 'courses/list.html', {
+        'categories': kategoriler,
+        'courses': kurslar,
+        'page_obj': page_obj,
     })
 
 
