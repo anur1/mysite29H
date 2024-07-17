59c1eea9859a653eab2ded0a903c3c4ce77e6547
A Nuri <ahmettonuri@gmail.com>
2024-07-17 14:33:23 +0300

    form field added

--- courses/forms.py
+++ courses/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 
 class CourseAddForm (forms.Form):
-    title = forms.CharField()
-    description = forms.CharField(widget =forms.Textarea)
-    imageUrl = forms.CharField()
-    slug = forms.CharField()
+    title = forms.CharField(
+        label = "kurs başlığı", 
+        required= True, 
+        error_messages={"required": "kurs başlığı girmelisiniz."    }, 
+        widget = forms.TextInput(attrs={"class": "form-control"}))
+    description = forms.CharField(label= "açıklamalar",   widget = forms.Textarea(attrs={"class": "form-control"}))
+    imageUrl = forms.CharField(label = "kurs resmi",  widget = forms.TextInput(attrs={"class": "form-control"}))
+    slug = forms.SlugField(label= "uzantı ismi"  , widget = forms.TextInput(attrs={"class": "form-control"}))
 
 
 
