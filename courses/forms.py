b8b709651a1067c72c5d74741b7d4facfa803ea3
A Nuri <ahmettonuri@gmail.com>
2024-07-17 15:07:47 +0300

    form for döngüsü

--- courses/forms.py
+++ courses/forms.py
@@ -1,14 +1,25 @@
 from django import forms
 
 class CourseAddForm (forms.Form):
+
     title = forms.CharField(
         label = "kurs başlığı", 
         required= True, 
-        error_messages={"required": "kurs başlığı girmelisiniz."    }, 
+        error_messages={"required": "kurs başlığı girmelisiniz."}, 
         widget = forms.TextInput(attrs={"class": "form-control"}))
-    description = forms.CharField(label= "açıklamalar",   widget = forms.Textarea(attrs={"class": "form-control"}))
-    imageUrl = forms.CharField(label = "kurs resmi",  widget = forms.TextInput(attrs={"class": "form-control"}))
-    slug = forms.SlugField(label= "uzantı ismi"  , widget = forms.TextInput(attrs={"class": "form-control"}))
+    
+    description = forms.CharField(label= "açıklamalar",
+                                  widget = forms.Textarea(attrs={"class": "form-control"}),
+                                  error_messages={"required": "açıklama girmelisiniz."},
+                                     )
+    imageUrl = forms.CharField(label = "kurs resmi",  
+                               widget = forms.TextInput(attrs={"class": "form-control"}),
+                                error_messages={"required": "resim uzantısı girmelisiniz."},
+                                )
+    slug = forms.SlugField(label= "uzantı ismi"  , 
+                           widget = forms.TextInput(attrs={"class": "form-control"}),
+                            error_messages={"required": "takma ad girmelisiniz."},
+                            )
 
 
 
