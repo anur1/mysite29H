ad27b0ec616be039061f577e74b48e62f355baeb
anur1 <ahmettonuri@gmail.com>
2024-07-24 16:20:31 +0300

    navbar login buttons added

---
+++ account/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import redirect, render
+
+def user_login (request,):
+    return render (request, "account/login.html", )
+
+def user_register (request,):
+    return render (request, "account/register.html", )
+
+def user_logout (request,):  #cookies sil, anasayfaya yönlendir. 
+    return redirect ("index" )
+
