0559f8dcf57acdabf87ed56cfc5a464eb3a06f4e
A Nuri <ahmettonuri@gmail.com>
2024-07-26 15:03:55 +0300

    register form added

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def user_login (request,):
     #kullanıcı henüz giriş yapmadı ise login sayfasına gitmesine gerek yok, index e gidebilir.
@@ -18,8 +19,33 @@ def user_login (request,):
     else:   
         return render (request, "account/login.html", )
 
+
+
 def user_register (request,):
-    return render (request, "account/register.html", )
+    if request.method=="POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password != repassword: 
+            return render (request, "account/register.html", {"error": "parlolalar eşleşmiyor", "username": username, "email": email})
+
+        if User.objects.filter(username= username).exists():
+                return render (request, "account/register.html", {"error": "aynı isimli bir kullanıcı daha var, başka bir isim seçiniz", "username": username, "email": email})
+        
+        if User.objects.filter(email=email).exists():
+            return render (request, "account/register.html", {"error": "aynı email adresli birisi daha var, başka bir email adresi giriniz..", "username": username, "email": email})
+
+        user = User.objects.create_user(username = username, email= email, password = password)
+        user.save()
+        return redirect("user_login")
+
+
+    else:
+        return render (request, "account/register.html", )
+
+
 
 def user_logout (request,):  #logout yap, cookies sil, anasayfaya yönlendir. 
     logout(request)
