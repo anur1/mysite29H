f4bee8b2bf334ab1053a65826a262aa3c902e771
A Nuri <ahmettonuri@gmail.com>
2024-07-29 15:25:58 +0300

    authentication form added

--- account/views.py
+++ account/views.py
@@ -2,31 +2,43 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def user_login (request,):
     #kullanıcı super admin değilse, url'deki next çıkar. Yani kullanıcı yetkisizdir. 
     if request.user.is_authenticated and "next" in request.GET: 
         return render(request, "account/login.html", {"error":"username ya da password yetkisiz alana giremez!"})
     
-    if (request.method=="POST"):
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            messages.add_message(request, messages.SUCCESS, "giriş başarılı")
-            #return redirect( "index"   )  next yoksa gitsin, varsa ilgili sayfaya gitsin
-            nextUrl = request.GET.get("next", None)
-            if nextUrl is None:
-                return redirect("index")
-            else: 
-                return redirect(nextUrl)
+    if request.method =="POST":
+        form = AuthenticationForm(request, data =request.POST)
+        
+        if form.is_valid():
+            #not: request.cleaned_data çalışmıyor. form.cleaned_data çalıştı. 
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+        #Authentication Form kullanmadan önce..
+        # username = request.POST["username"]
+        # password = request.POST["password"]
+        # user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.add_message(request, messages.SUCCESS, "giriş başarılı")
+                #return redirect( "index"   )  next yoksa gitsin, varsa ilgili sayfaya gitsin
+                nextUrl = request.GET.get("next", None)
+                if nextUrl is None:
+                    return redirect("index")
+                else: 
+                    return redirect(nextUrl)
+                
+            else:
+                return render(request, "account/login.html", {"form": form} )
         else:
-            messages.add_message(request, messages.ERROR, "giriş başarısız, username ya da password yanlış")
-            return render(request, "account/login.html", )
+            #messages.add_message(request, messages.ERROR, "giriş başarısız, username ya da password yanlış") bu hata mesajına artık gerek yok. form dan geliyor zaten
+            return render(request, "account/login.html", {"form": form} )
     else:   
-        return render (request, "account/login.html", )
+        form = AuthenticationForm()
+        return render (request, "account/login.html", {"form": form})
 
 
 
